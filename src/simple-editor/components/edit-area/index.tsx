89ac2d360d374e2c8c2f1f893d437018614f1318
ZhouRenGui <729049182@qq.com>
2021-11-22 21:05:12 +0800

    fix: 修改文档，解决hooks依赖警告问题

--- src/simple-editor/components/edit-area/index.tsx
+++ src/simple-editor/components/edit-area/index.tsx
@@ -1,4 +1,3 @@
-// TODO: 优化
 import { useRef, memo, useLayoutEffect } from "react";
 import { fromEvent } from "rxjs";
 import { iframeScript } from "./iframe-script";
@@ -13,27 +12,26 @@ const EditArea = (props: EditAreaProps) => {
   const iframeRef = useRef<HTMLDivElement>(null);
 
   useLayoutEffect(() => {
-    if (iframeRef?.current?.firstChild) {
-      iframeRef.current?.firstChild.remove();
-    }
-    const iframe = document.createElement("iframe");
-    iframe.src = iframeScript(content);
-    iframe.style.width = "100%";
-    iframe.style.border = "none";
-    iframe.style.display = "black";
-    iframe.style.minHeight = "100%";
-    if (iframeRef.current) {
-      iframeRef.current.appendChild(iframe);
-    }
-    fromEvent(window, "message").subscribe((e: Event) => {
-      const event = e as MessageEvent;
-      if (event.data === "complete") {
-        if (iframe.contentDocument) {
-          onIframeMounted(iframe.contentDocument);
-        }
+    if (!iframeRef?.current?.firstChild) {
+      const iframe = document.createElement("iframe");
+      iframe.src = iframeScript(content);
+      iframe.style.width = "100%";
+      iframe.style.border = "none";
+      iframe.style.display = "black";
+      iframe.style.minHeight = "100%";
+      if (iframeRef.current) {
+        iframeRef.current.appendChild(iframe);
       }
-    });
-  }, []);
+      fromEvent(window, "message").subscribe((e: Event) => {
+        const event = e as MessageEvent;
+        if (event.data === "complete") {
+          if (iframe.contentDocument) {
+            onIframeMounted(iframe.contentDocument);
+          }
+        }
+      });
+    }
+  }, [content, onIframeMounted]);
 
   return <div className="edit-area" ref={iframeRef}></div>;
 };
--- src/simple-editor/index.tsx
+++ src/simple-editor/index.tsx
@@ -17,36 +17,39 @@ const SimpleEditor: React.FC<SimpleEditorProps> = (props) => {
   const [selection, setSelection] = useState<EditorSelection>();
   const [editorContent, setEditorContent] = useState<EditorContent>();
 
-  const onIframeMounted = useCallback((contentDocument: Document) => {
-    if (!selection) {
-      const nextSelection = new EditorSelection(contentDocument);
-      const nextEditorContent = new EditorContent(contentDocument);
-      setSelection(nextSelection);
-      setEditorContent(nextEditorContent);
-      nextSelection.subscribe((selection: EditorSelection) => {
-        // 富文本光标变化时触发
-      });
-      nextEditorContent.subscribe(() => {
-        // 编辑器内容变化触发
-        onChange?.(contentDocument?.body?.innerHTML);
-      });
-      // contentDocument.addEventListener("paste", (ev) => {
-      //   // 粘贴时处理
-      //   let clipboardData = ev.clipboardData;
-      //   let content = clipboardData?.getData("text/html");
-      //   // 解析HTML
-      //   const parser = new htmlparser2.Parser({
-      //     onopentag(tagname, attribs) {
-      //       if (tagname === "table") {
-      //         attribs.style = "";
-      //       }
-      //     },
-      //   });
-      //   parser.write(content as string);
-      //   parser.end();
-      // });
-    }
-  }, []);
+  const onIframeMounted = useCallback(
+    (contentDocument: Document) => {
+      if (!selection) {
+        const nextSelection = new EditorSelection(contentDocument);
+        const nextEditorContent = new EditorContent(contentDocument);
+        setSelection(nextSelection);
+        setEditorContent(nextEditorContent);
+        nextSelection.subscribe(() => {
+          // 富文本光标变化时触发
+        });
+        nextEditorContent.subscribe(() => {
+          // 编辑器内容变化触发
+          onChange?.(contentDocument?.body?.innerHTML);
+        });
+        // contentDocument.addEventListener("paste", (ev) => {
+        //   // 粘贴时处理
+        //   let clipboardData = ev.clipboardData;
+        //   let content = clipboardData?.getData("text/html");
+        //   // 解析HTML
+        //   const parser = new htmlparser2.Parser({
+        //     onopentag(tagname, attribs) {
+        //       if (tagname === "table") {
+        //         attribs.style = "";
+        //       }
+        //     },
+        //   });
+        //   parser.write(content as string);
+        //   parser.end();
+        // });
+      }
+    },
+    [onChange, selection]
+  );
 
   useEffect(() => {
     return () => {
